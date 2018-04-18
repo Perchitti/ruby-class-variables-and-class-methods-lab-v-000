e02524d92873cd70eb1d64bfb775d2f2dc6c2ff2
Lauren Perchitti <laurenperchitti@gmail.com>
2018-04-18 17:20:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,12 +4,14 @@ class Song
 attr_accessor :name, :artist, :genre
 
 @@COUNT = 0
+@@ARTISTS = []
 
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
 end
+
 
 
 end
