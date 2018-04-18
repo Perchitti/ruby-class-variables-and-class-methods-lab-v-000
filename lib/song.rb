6ca53b9f530905fc46f0386ad4172022fe2baeea
Lauren Perchitti <laurenperchitti@gmail.com>
2018-04-18 19:05:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,14 @@ def self.genre_count
   @@genre_count
 end
 
+def self.artist_count
+  @@artist_count = {}
+  @@artists.each do |artist|
+    @@artist_count[artist] = @@artists.count(artist)
+  end
+  @@artist_count
+end
+
+
+
 end
